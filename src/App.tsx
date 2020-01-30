f751859d3ce7954bf035ff5ba5350799996cc310
Chris Flannery <chriswillsflannery@gmail.com>
2020-01-30 13:04:13 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './components/about';
+import BuyAndSell from './components/buyandsell';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav className="navbar">
+            <div className="Logo">Triple Point <span>Liquidity</span></div>
+            <ul className="nav-ul">
+              <li><Link to={'/about'} className="nav-link"> About </Link></li>
+              <li><Link to={'/buyandsell'} className="nav-link"> Buy and Sell </Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/about' component={About} />
+            <Route path='/buyandsell' component={BuyAndSell} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/buyandsell.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BuyAndSell extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Buy & Sell</h2>
+      </div>
+    )
+  }
+}
+
+export default BuyAndSell;
