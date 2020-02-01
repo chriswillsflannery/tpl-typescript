38239744a8e9edce4c1cee7e767c3f77f7fedf39
Chris Flannery <chriswillsflannery@gmail.com>
2020-01-31 21:57:07 -0500

    converting to hooks

--- src/components/buyGrid.tsx
+++ src/components/buyGrid.tsx
@@ -1,41 +1,49 @@
 import React from 'react';
 
-class BuyGrid extends React.Component {
-  render() {
-    return (
-      <main className="buyGrid">
-        <div className="buyHeader">
-          <h2>My Deals</h2>
+interface Props {
+  name: string;
+}
+
+const BuyGrid = (props: Props) => {
+  return (
+    <main className="buyGrid">
+      <div className="buyHeader">
+        <h2>{props.name}</h2>
+      </div>
+      <div className="info">
+        <div className="info-line descriptors">
+          <h3>DEAL ID</h3>
+          <h3>ASSET</h3>
         </div>
-        <div className="info">
-          <div className="info-line descriptors">
-            <h3>DEAL ID</h3>
-            <h3>ASSET</h3>
+        <div className="info-line listings">
+          <div className="listing">
+            <h3 className="listing-attribute">A12345</h3>
+            <h3 className="listing-attribute">HTT</h3>
+            <h3 className="listing-link">
+              <a href="#">VIEW BIDS</a>
+            </h3>
           </div>
-          <div className="info-line listings">
-            <div className="listing">
-              <h3>A12345</h3>
-              <h3>HTT</h3>
-              <h3>VIEW BIDS</h3>
-            </div>
-            <div className="listing">
-              <h3>A12345</h3>
-              <h3>HTT</h3>
-              <h3>VIEW BIDS</h3>
-            </div>
-            <div className="listing">
-              <h3>A12345</h3>
-              <h3>HTT</h3>
-              <h3>VIEW BIDS</h3>
-            </div>
+          <div className="listing">
+            <h3 className="listing-attribute">A12345</h3>
+            <h3 className="listing-attribute">HTT</h3>
+            <h3 className="listing-link">
+              <a href="#">VIEW BIDS</a>
+            </h3>
           </div>
-          <div className="info-line createNew">
-            <h3>CREATE DEAL</h3>
+          <div className="listing">
+            <h3 className="listing-attribute">A12345</h3>
+            <h3 className="listing-attribute">HTT</h3>
+            <h3 className="listing-link">
+              <a href="#">VIEW BIDS</a>
+            </h3>
           </div>
         </div>
-      </main>
-    )
-  }
+        <div className="info-line createNew">
+          <h3><a href="#">CREATE DEAL</a></h3>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default BuyGrid;
--- src/components/buyandsell.tsx
+++ src/components/buyandsell.tsx
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BuyGrid from './buyGrid';
 import './buyandsell.css';
 
-class BuyAndSell extends React.Component {
-  render() {
-    return (
-      <div className="buyandsell">
-        <BuyGrid />
-        <BuyGrid />
-      </div>
-    )
-  }
+
+// this.state = {
+//   myDeals: [{
+//     dealID: 'A12345',
+//     asset: 'HTT'
+//   }, {
+//     dealID: 'B34567',
+//     asset: 'PSM'
+//   }, {
+//     dealID: 'C56789',
+//     asset: 'PPT'
+//   }],
+//   marketPlace: [{
+//     dealID: 'D23456',
+//     asset: 'APP'
+//   }, {
+//     dealID: 'E45678',
+//     asset: 'LEG'
+//   }, {
+//     dealID: 'F89123',
+//     asset: 'OOG'
+//   }]
+
+
+// type BuyState = {
+//   myDeals: object[],
+//   marketPlace: object[]
+// }
+
+const BuyAndSell: React.FC = () => {
+  const [myDeals, setMyDeals] = useState([] as any);
+  const [marketPlace, setMarketPlace] = useState([] as any);
+
+  useEffect(() => {
+    setMyDeals([
+      ...myDeals,
+      {
+        dealID: 'A12345',
+        asset: 'HTT'
+      }, {
+        dealID: 'B34567',
+        asset: 'PSM'
+      }, {
+        dealID: 'C56789',
+        asset: 'PPT'
+      }
+    ]);
+    setMarketPlace([
+      ...marketPlace,
+      {
+        dealID: 'D23456',
+        asset: 'APP'
+      }, {
+        dealID: 'E45678',
+        asset: 'LEG'
+      }, {
+        dealID: 'F89123',
+        asset: 'OOG'
+      }
+    ])
+  }, [marketPlace, myDeals])
+
+  return (
+    <div className="buyandsell">
+      <BuyGrid name="My Deals" />
+      <BuyGrid name="Marketplace" />
+    </div>
+  )
 }
 
 export default BuyAndSell;
